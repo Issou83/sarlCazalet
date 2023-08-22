bb6ed77ca6418681bf98d3f0dbd95bcc8a891a7a
Issou83 <issou8@gmail.com>
2023-08-22 13:39:32 +0200

    maj

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import "./index.css";
 
 function Services() {
-  return <div><p>Services</p></div>;
+  return <div className="mainServices">
+    <h1>NOS SERVICES</h1>
+    <p className="descriServices">Implantée à Sévignacq-Meyracq, SARL Cazalet met à votre service une équipe de Professionnels qualifiés, pour la réalisation de vos traveaux de vidange de fosses septiques, bacs à graisses, postes de relevages, ainsi que tout vos systèmes d'assainissement.
+      <br/>Nous déployons une camera vidéo performante et un materiel de pointepour effectuer l'inspection, avant d'entamer le curage et débouchage des canalisations obstruées. Pour répondre à tous vos besoins, notre entreprise propose également la location de materiels divers pour organisation d' événemnts, de chantiers, d'entretiens...  </p></div>;
 }
 
 export default Services;
---
+++ src/composants/Cookies/cookies.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+
+const cookies = () => {
+  return (
+    <div>
+      <p>
+        <p className="cookiesTitle">
+          Politique de gestion des cookies de SARL Cazalet à Arudy
+        </p>
+        <p className="cookiesTitleSec">Cookies essentiels.</p>
+        <p>
+          Ils sont strictement nécessaires à l'utilisation du service et
+          permettent l'utilisation des principales fonctionnalités du site,
+          comme le cas échéant l'accés à votre compte personnel, ou encore de
+          mémoriser les préférences d'affichage de votre terminal (langue,
+          paramètres d'affichage) et d'en tenir compte lors de vos visites,
+          selon la charte graphique et les logiciels de visualisation ou de
+          lecture de votre terminal ils peuvent inclure des cookies de réseaux
+          sociaux lorsque vous interagissez avec ces derniers. Ils nous
+          permettent aussi de lier entre elles les différentes pages consultées
+          pour vous assurer une navigation fluide <br />
+          Pour paramétrer, vous pouvez désactiver complètement les cookies dans
+          votre navigateur. Dans ce cas notre site ne fonctionnera plus
+          normalement mais vous pourrez quand même effectuer des actionsbasiques
+        </p>
+      </p>
+      <ul>
+        <li>
+          1/ Si vous utilisez le navigateur Internet Explorer Dans Internet
+          Explorer, cliquez sur le bouton Outils, puis sur «Options Internet». -
+          Sous l'onglet Confidentialité déplacez le curseur vers le haut pour
+          bloquer tous les cookies ou vers le bas pour autoriser tous les
+          cookies, puis cliquez sur OK.
+        </li>
+        <li>
+          2/ Si vous utilisez le navigateur Firefox - Allez dans le menu Outils
+          du navigateur puis sélectionnez le menu Options - Cliquez sur l'onglet
+          evie privée, décocher la case Accepter les cookies puis cliquez sur OK
+        </li>
+        <li>
+          3/ Si vous utilisez le navigateur Safari - Dans votre navigateur, cho
+          aissez le menu Edition - puis selectionnez Préférences - Cliquez sur
+          Confidentialité. - Positionnez le réglage Bioquer les cookies sur
+          Toujours puis cliquez sur OK
+        </li>
+        <li>
+          4/ Si vous utilisez le navigateur Google Chrome - Cliquez sur le menu
+          Chrome dans la barre d'outils du navigateur - Sélectionnez Paramètres
+          - Cliquez sur Afficher les paramètres avances - Dans la section
+          Confidentialité, cliquez sur le bouton Paramètres de contenu - Dans la
+          section Cookies, sélectionnez Interdire à tous les sites de stocker
+          des données et cochez la case Bloquer les cookies et les données de
+          site ters, puis cliquez sur C
+        </li>
+      </ul>
+      <p className="cookiesTitleSec">Cookies fonctionnels de mesure d'audience et statistiques.</p>
+      <p>Il nous permettent de faire vivre le site et d'établi des statistiques et comptages de frequentation et d'utisation de ses rubriques et contenues, pour réaliser des études afin d'améliorer le contenu (mesure du nombre de visites,
+         de pages vues ou encore de l'activité des visiteurs sur le site et de leur Fréquence de retour).
+        Ils nous permettent également d'analyser la navigation des internautes afin d'améliorer notre service ou de détecter des dysfonctionnements.
+        Nous déposons également des cookies de mesure d'audience exemptés de consentement grace à notre outil de mesure d'audience AT Internet. Les données collectées dans ce cadre ne
+        permettent pas de suivre votre magation sur d'autres sites tiers
+        Vous pouvez désactiver ces cookies exemptes de consentergent en cochant la case suivante O
+        </p>
+        <p className="cookiesTitleSec">Cookies de commercialisation.</p>
+        <p>ils sont susceptibles d'être placés dans votre terminal, afin d'identifier vos centres d'interêt au travers des produits consultés sur notre site et de collecter des données de navigation afin de personnaliser l'offre publicitaire qui vous est adressée et en dehors de nos sites.
+            Vous pouvez vous opposer à l'usage de vos données de navigation à des fins publicitaires par nos partenaires
+            Vous pouvez galament vous opposer à tout les cookies bars depuit votre navigateur
+        </p>
+        <ul>
+            <li>1/ Si vous utilisez un navigateur internet Explorer
+
+                Dansinternet Explore cliquez te bouton Ounis, pois sur Options internet
+
+Sous fonglet Confidentialite sous Cookie activer loption isloquer tous les cookies ties, puis cliquez sur ÜK</li>
+            <li></li>
+            <li></li>
+            <li></li>
+        </ul>
+
+    </div>
+  );
+};
+
+export default cookies;
