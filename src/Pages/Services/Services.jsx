b3a4454252a62a1f83cda98ff532ee1f47e195eb
Issou83 <issou8@gmail.com>
2023-10-17 23:53:14 +0200

    MAJ

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -6,7 +6,7 @@ function Services() {
     <h1>NOS SERVICES</h1>
       <p className="descriServices">Implantée à Arudy, SARL Cazalet met à votre service une équipe de professionnels qualifiés, pour la réalisation de vos travaux, de vidange de fosses septiques, bacs à graisses, postes de relevages, ainsi que tout vos systèmes d'assainissement.
       </p>
-      <p className="camExlication">Nous déployons une camera vidéo performante et un materiel de pointe pour effectuer l'inspection, avant d'entamer le curage et débouchage des canalisations obstruées. Pour répondre à tous vos besoins, notre entreprise propose également la location de materiels divers pour organisation d'événemnts, de chantiers, d'entretiens...  
+      <p className="camExplication">Nous déployons une camera vidéo performante et un materiel de pointe pour effectuer l'inspection, avant d'entamer le curage et débouchage des canalisations obstruées. Pour répondre à tous vos besoins, notre entreprise propose également la location de materiels divers pour organisation d'événemnts, de chantiers, d'entretiens...  
       </p>
       </div>;
 }
