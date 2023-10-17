6389bae4fe166c2dd7028ec24482cc926448a2ea
Issou83 <issou8@gmail.com>
2023-10-18 00:42:33 +0200

    MAJ

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -77,7 +77,7 @@ const Contact = () => {
       <div 
       className="sectionMap">
       <h2>Notre rayon d'action</h2>
-      <MapContainer center={position} zoom={8} style={{ height: "300px", width: "100%" }}>
+      <MapContainer center={position} zoom={8} style={{ height: "300px", width: "50%" }}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
